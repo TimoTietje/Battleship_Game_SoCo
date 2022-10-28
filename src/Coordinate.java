6b0be15488e9a5ea04289655863fa809a4d82d1e
Felix Merz <felixmerz00@gmail.com>
2022-10-28 13:44:43 +0200

    Constructor and getter methods created

--- src/Coordinate.java
+++ src/Coordinate.java
@@ -1,4 +1,19 @@
 public class Coordinate {
     private int X;
     private int Y;
+    // Constructor
+    public Coordinate(int x, int y){
+        X = x;
+        Y = y;
+    }
+
+    public int getX() {
+        return X;
+    }
+
+    public int getY() {
+        return Y;
+    }
+    /* I think this class doesn't need setter methods,
+    you only need to set the ship once (when you create them) */
 }
