f31a3709a5b6fe9bf980041c2be964bc573f3f98
Felix Merz <felixmerz00@gmail.com>
2022-10-28 13:51:46 +0200

    Instance variables and constructor created

--- src/Carrier.java
+++ src/Carrier.java
@@ -1,2 +1,8 @@
 public class Carrier {
+    private int length;
+    private Coordinate startCoordinate;
+    public Carrier(int l, int start_x, int start_y){
+        length = l;
+        Coordinate startCoordinate = new Coordinate(start_x, start_y);
+    }
 }
