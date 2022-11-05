786f7bf2b8c374d7616a94c9453109d0040dd89a
felixmerz <65616000+felixmerz00@users.noreply.github.com>
2022-11-05 20:40:04 +0100

    not much

--- src/main/Coordinate.java
+++ src/main/Coordinate.java
@@ -1,7 +1,7 @@
 public class Coordinate {
     private int x;
     private int y;
-    // Constructor
+
     public Coordinate(int x, int y){
         this.x = x;
         this.y = y;
@@ -14,6 +14,4 @@ public class Coordinate {
     public int getY() {
         return y;
     }
-    /* I think this class doesn't need setter methods,
-    you only need to set the ship once (when you create them) */
 }
